2eb02b101e81c1118def7e4df4ffa26cdaebf7e4
João Mangueira <jamjs7@gmail.com>
2024-06-19 18:59:01 -0300

    add auth

--- apps/partner2/src/eventos/eventos.controller.ts
+++ apps/partner2/src/eventos/eventos.controller.ts
@@ -1,8 +1,9 @@
-import {Controller, Get, Post, Body, Patch, Param, Delete, HttpCode} from '@nestjs/common';
+import {Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, UseGuards} from '@nestjs/common';
 import { EventsService } from '@app/core/events/events.service';
 import { CriarEventoRequest } from './request/criar-evento.request';
 import { AlterarEventosRequest } from './request/alterar-eventos.request';
 import {ReservarAssentoRequest} from "./request/reservar-assento.request";
+import {AuthGuard} from "@app/core/auth/auth.guard";
 
 @Controller('eventos')
 export class EventosController {
@@ -34,6 +35,7 @@ export class EventosController {
     return this.eventsService.remove(id);
   }
 
+  @UseGuards(AuthGuard)
   @Post(':id/reserve')
   reserveSpots(@Param('id') id: string,  @Body() dto: ReservarAssentoRequest) {
     return this.eventsService.reserveSpot(id, dto);
--- apps/partner2/src/partner2.module.ts
+++ apps/partner2/src/partner2.module.ts
@@ -3,11 +3,13 @@ import {ConfigModule} from "@nestjs/config";
 import {EventosModule} from "./eventos/eventos.module";
 import {AssentosModule} from "./assentos/assentos.module";
 import {PrismaModule} from "@app/core/prisma/prisma.module";
+import {AuthModule} from "@app/core/auth/auth.module";
 
 @Module({
   imports: [
-    ConfigModule.forRoot({envFilePath: '.env.partner2'}),
+    ConfigModule.forRoot({envFilePath: '.env.partner2', isGlobal: true}),
     PrismaModule,
+    AuthModule,
     EventosModule,
     AssentosModule
   ],
